491494cd555678f8d1793dff69b663b37cb214ea
joshuahernn <JoshuaHernn@gmail.com>
2026-03-01 21:45:26 -0800

    Debugging hitboxes, adding in sprites.
    This version has an issue where hitboxes are set to rect when should be set to new sprites.
    
    - Fixed PLAYER paddle hitbox, however reflection mechanics need to be adjusted.
    - Opp. is still using rect hitbox.
    - Ball sprite needs to be inputted.

--- pong_game/paddle.py
+++ pong_game/paddle.py
@@ -2,10 +2,27 @@ import pygame
 
 class Paddle:
     def __init__(self, x, screen_height, width=15, height=100):
-        self.rect = pygame.Rect(0, 0, width, height)
-        self.rect.x = x
-        self.rect.centery = screen_height // 2
-        self.color = (255, 255, 255)
+
+        self.image = pygame.image.load("assets/newplayerPaddle.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (128,128))
+
+        self.rect = self.image.get_rect()
+        self.rect.left = x 
+        self.rect.centery = screen_height // 2 
+
+        self.mask = pygame.mask.from_surface(self.image)
+        
+    # DEFAULT PADDLES. 
+       # self.rect = pygame.Rect(0, 0, width, height)
+       # self.rect.x = x
+       # self.rect.centery = screen_height // 2
+       # self.color = (255, 255, 255)
+    
+   
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect)
+        pygame.draw.rect(screen, (0, 255, 0), self.rect, 2) # debug hitbox
+        
+        screen.blit(self.image, self.rect) 
+
+        
