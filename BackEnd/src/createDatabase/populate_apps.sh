6ab160f55565d16a1a97e3804058818c28b5831f
Nick Yoder <nicholas.yoder@txse.com>
2026-04-28 17:56:23 -0700

    Improved startup, need descriptions from everyone.

--- BackEnd/src/createDatabase/populate_apps.sh
+++ BackEnd/src/createDatabase/populate_apps.sh
@@ -4,3 +4,33 @@ curl -X POST http://127.0.0.1:5000/api/application \
   -F "description=Dodge the bullets and shoot back. How long can you survive?" \
   -F "app_zip=@./appsToUpload/BulletHell.zip"
 
+curl -X POST http://127.0.0.1:5000/api/application \
+  -F "name=2D Parkour Game" \
+  -F "release_date=2026-054-27" \
+  -F "description=" \
+  -F "app_zip=@./appsToUpload/2DParkourGame.zip"
+
+curl -X POST http://127.0.0.1:5000/api/application \
+  -F "name=Clicker Game" \
+  -F "release_date=2026-054-27" \
+  -F "description=" \
+  -F "app_zip=@./appsToUpload/ClickerGame.zip"
+
+curl -X POST http://127.0.0.1:5000/api/application \
+  -F "name=Mines" \
+  -F "release_date=2026-054-27" \
+  -F "description=" \
+  -F "app_zip=@./appsToUpload/Mines.zip"
+
+curl -X POST http://127.0.0.1:5000/api/application \
+  -F "name=Pong" \
+  -F "release_date=2026-054-27" \
+  -F "description=" \
+  -F "app_zip=@./appsToUpload/Pong.zip"
+
+curl -X POST http://127.0.0.1:5000/api/application \
+  -F "name=Text Adventure" \
+  -F "release_date=2026-054-27" \
+  -F "description=" \
+  -F "app_zip=@./appsToUpload/TextAdventure.zip"
+
